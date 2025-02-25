93dea7e59ce222de387fe5ea1fed40c44e6a6c8b
LudCodes <lud.coutinho15@gmail.com>
2025-02-25 14:56:33 -0300

    Nomeação

---
+++ aula1_variaveisTipos/variaveis_tipos.js
@@ -0,0 +1,39 @@
+const n = 3;
+
+//n = 5 : Nao pode atribuir um valor a uma variavel constante.
+
+var j = 2;
+var j = 3; // o var permite que redeclare uma variavel.
+
+//console.log('j='+j);
+
+let i = 4; //o let permite que a variavel seja alterada, mas não redeclarada.
+i = 6;
+
+//console.log('i='+i);
+
+let nome = "Carlos";
+let cpf = '14785';
+let telefone = "2589";
+
+let tString = `Meu nome é ${nome}. Meu cpf é ${cpf}. Telefone: ${telefone}`;
+
+//console.log(minhaString);
+
+let x = 5.0;
+x = .5;
+x = 3;
+
+//console.log(typeof x);
+//console.log(typeof tString);
+
+let verdadeiro = true;
+let falso = false;
+
+let conexao = null;
+let conexao2;
+
+let y = x * x;
+
+console.log('y='+y);
+
