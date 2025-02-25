8f146b66ccd8f59e11c3464d19c9e86e72857445
LudCodes <lud.coutinho15@gmail.com>
2025-02-25 16:20:28 -0300

    atividade da aula1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,31 @@
+// adição (+)
+
+let numA_adicao = 10;
+let numB_adicao = 4;
+let resultado_adicao = numA_adicao + numB_adicao;
+
+console.log (`O resultado da adição entre ${numA_adicao} e ${numB_adicao} é: ${resultado_adicao}`);
+
+// subtração (-)
+
+let numA_subtracao = 10;
+let numB_subtracao = 10;
+let resultado_subtracao = numA_subtracao - numB_subtracao;
+
+console.log (`O resultado da subtração entre ${numA_subtracao} e ${numB_subtracao} é: ${resultado_subtracao}`);
+
+// multiplicação (*)
+
+let numA_multiplicacao = 6;
+let numB_multiplicacao = 10;
+let resultado_multiplicacao = numA_multiplicacao * numB_multiplicacao;
+
+console.log (`O resultado da multiplicação entre ${numA_multiplicacao} e ${numB_multiplicacao} é: ${resultado_multiplicacao}`);
+
+// divisão (/)
+
+let numA_divisao = 6;
+let numB_divisao = 10;
+let resultado_divisao = numA_divisao / numB_divisao;
+
+console.log (`O resultado da divisão entre ${numA_divisao} e ${numB_divisao} é: ${resultado_divisao}`);
